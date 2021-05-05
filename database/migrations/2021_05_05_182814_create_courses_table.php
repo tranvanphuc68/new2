9b2feecb114287db1bce5a7d6a2072d6eb628e93
Sunshine <you@example.com>
2021-05-06 02:05:46 +0700

    Create table courses,students_courses,detail_classes,report_teachers,marks,fees

---
+++ database/migrations/2021_05_05_182814_create_courses_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('id_teacher')->unsigned();
+            $table->string('description');
+            $table->string('timetable');
+            $table->integer('max_students');
+            $table->integer('sum_time');
+            $table->integer('lessons');
+            $table->double('fee',9,0);
+            $table->double('salary',9,0);
+            $table->integer('status');
+            $table->timestamps();
+            
+            $table->foreign('id_teacher')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
---
+++ database/migrations/2021_05_05_184008_create_marks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->bigInteger('id_course')->unsigned();
+            $table->bigInteger('id_student')->unsigned();
+            $table->float('mark',2,1);
+            $table->timestamps();
+
+            $table->foreign('id_course')->references('id')->on('courses');
+            $table->foreign('id_student')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
