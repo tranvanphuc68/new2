5ac3d14784b9d087ed8d482cbd64f06ce52bb712
Sunshine <nangh201@gmail.com>
2021-05-15 23:34:12 +0700

    update

--- resources/views/marks/show.blade.php
+++ resources/views/marks/show.blade.php
@@ -7,20 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    @if (Auth::user()->role == 'Teacher')
+    @if (count($students) != 0)
+        @if (Auth::user()->role == 'Teacher')
         <a href="{{ url("/marks/edit/{$students[0]->id_course}") }}">Edit</a>
+        @endif
+        <h1>{{ $students[0]->name }}</h1>
+        <table>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->id_student }}</td>
+                    <td>{{ $student->fullname }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->mark }}</td>
+                </tr>
+            @endforeach
+        </table>
+    @else
+    <p>Null</p>
     @endif
-    <h1>{{ $students[0]->name }}</h1>
-    <table>
-        @foreach($students as $student)
-            <tr>
-                <td>{{ $student->id_student }}</td>
-                <td>{{ $student->fullname }}</td>
-                <td>{{ $student->name }}</td>
-                <td>{{ $student->mark }}</td>
-            </tr>
-        @endforeach
-    </table>
+    
+    
 
 </body>
 </html>
